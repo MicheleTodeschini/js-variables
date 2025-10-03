3c809c46b18134e06fd5cd644f3d477f08897701
MicheleTodeschini <micheletodeschini01@gmail.com>
2025-10-03 14:40:48 +0200

    solved topic 2 es 2

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -11,6 +11,19 @@ console.log(name);
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
 
+let number = '4'
+console.log(number);
+
+
+let text = 'ciao a tutti'
+console.log(text);
+
+
+let bool = true;
+console.log(bool);
+
+
+
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
 // in console la stringa con il nome completo
